d310cca24e9176b960c213090924fa5974059a8f
amirlanenkhtur <e.ami.prof@gmail.com>
2026-03-24 23:17:21 -0400

    fixed MondoDB connection

--- server/server.js
+++ server/server.js
@@ -2,25 +2,35 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
+const cors = require('cors');
+app.use(cors());
+
 app.use(express.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/urlShortener')
-  .then(() => console.log("MongoDB connected"))
+mongoose.connect('mongodb+srv://admin:AMI4ever@cluster0.r10rhdh.mongodb.net/?appName=Cluster0')
+  .then(() => console.log("MongoDB Atlas connected"))
   .catch(err => console.log(err));
 
 const urlRoutes = require('./routes/url');
 app.use('/api', urlRoutes);
 
 const Url = require('./models/Url');
+app.get('/', (req, res) => {
+  res.send("URL Shortener API is running 🚀");
+});
 
 app.get('/:code', async (req, res) => {
-    const url = await Url.findOne({ shortCode: req.params.code });
+    try {
+        const url = await Url.findOne({ shortCode: req.params.code });
 
-    if (url) {
-        return res.redirect(url.originalUrl);
-    } else {
-        return res.status(404).send("URL not found");
+        if (url) {
+            return res.redirect(url.originalUrl);
+        } else {
+            return res.status(404).send("URL not found");
+        }
+    } catch (err) {
+        res.status(500).send("Server error");
     }
 });
 
-app.listen(5000, () => console.log("Server running on port 3000"));
+app.listen(5000, () => console.log("Server running on port 5000"));
