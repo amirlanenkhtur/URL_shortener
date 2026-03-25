a51395204abefad20b17b1b20ab0c7d3521e7169
amirlanenkhtur <e.ami.prof@gmail.com>
2026-03-25 00:36:04 -0400

    .

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ app.use(cors());
 
 app.use(express.json());
 
-mongoose.connect('process.env.MONGO_URI')
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Atlas connected"))
   .catch(err => console.log(err));
 
